c9e58373384c7fc10d4b98c9e4059db55047240d
Jonathan Rozen <j.rozen@student.tudelft.nl>
2021-12-06 18:48:12 +0100

    Docker fix, plus small change

--- executive/actions/assign.py
+++ executive/actions/assign.py
@@ -1,4 +1,4 @@
-from executive.actions.models import AssignedAction
+from executive.actions.models import Action, AssignedAction
 from executive.actions.add import AddAction
 
 class AssignAction(AddAction):
@@ -8,8 +8,16 @@ class AssignAction(AddAction):
         action = AssignedAction(**options) 
         action.save()
 
-if __name__ == "__main__":
-    AssignAction().run()
+    def _get_assigned_to(self, name):
+        if len(Action) < 1:
+            print("Add some actions first")
+        assigned = AssignedAction.select().filter(AssignedAction.name==name).order_by(AssignedAction.id.asc())
+        if len(assigned) < 1:
+            print("No action has been assigned to this person!")
+        print("The Tasks assigned to {} are:".format(name))
+        for a in assigned:
+            print("{}, {}".format(a.id, Action.select().filter(Action.id==a.action_id)[0].name))
+
 
 
 
